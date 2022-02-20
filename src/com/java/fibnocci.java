7aa8047f2e3d8662462ee6e973a5b9087390e96c
Ranidhumma <ranidhumma8@gmail.com>
2022-02-20 18:33:58 +0530

    fibonacci

--- src/com/java/fibnocci.java
+++ src/com/java/fibnocci.java
@@ -1,10 +1,34 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class fibnocci {
 
+	
+	public static void fibonacci()
+	{    
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the range");
+		int range = scanner.nextInt();
+		System.out.println("the fibonacci series with in range are");
+		 int n1=0,n2=1,n3; int i;    
+		 System.out.print(n1+" "+n2);//printing 0 and 1    
+		    
+		 for(i=2; i<range; ++i)//loop starts from 2 because 0 and 1 are already printed    
+		 {    
+		  n3=n1+n2;    
+		  System.out.print(" "+n3);    
+		  n1=n2;    
+		  n2=n3;    
+		 }    
+		  
+	scanner.close();
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
      System.out.println("Welcome");
+     fibonacci();
+     
+   
 	}
 
 }
